204ade573cb41926571cffe082e63860efc51c5d
Gustav Wiberg <gustav.wiberg@gmail.com>
2024-10-09 18:00:22 +0200

    a

--- test/test_cv_data.py
+++ test/test_cv_data.py
@@ -1,38 +1,28 @@
 
 
-from ec4py import cv_data
-from ec4py.util import Quantity_Value_Unit as QVU
-#"import inc_dec    # "The code to test
-import numpy as np
 from pathlib import Path
 
 import unittest   # The test framework
 
-E =np.array([1,2,3])
-paths = []
-path_to_dataSetFolder = Path(".").cwd().parent.parent / "test_data" /"CV"
-print(path_to_dataSetFolder)
-#paths.append( path_to_dataSetFolder / "CV_144913_ 3.tdms")
-paths.append( path_to_dataSetFolder / "CV_153559_ 3.tdms")
-paths.append( path_to_dataSetFolder / "CV_153541_ 3.tdms")
-paths.append( path_to_dataSetFolder / "CV_153333_ 3.tdms")
-paths.append( path_to_dataSetFolder / "CV_151300_ 3.tdms")
-paths.append( path_to_dataSetFolder / "CV_151725_ 3.tdms")
-paths.append( path_to_dataSetFolder / "CV_151512_ 3.tdms")
 
-class Test_CV_Data(unittest.TestCase):
+
+class test_cv_data( unittest.TestCase ):
     
-    def load_files(self):
-        for path in paths:
-            Path(".").exists
-            self.assertEqual(path.exists, True)
+    def Test_aa(self):
         
+        paths = []
+        path_to_dataSetFolder = Path(".").cwd().parent.parent / "test_data" /"CV"
+        print(path_to_dataSetFolder)
+        #paths.append( path_to_dataSetFolder / "CV_144913_ 3.tdms")
+        paths.append( path_to_dataSetFolder / "CV_153559_ 3.tdms")
+        paths.append( path_to_dataSetFolder / "CV_153541_ 3.tdms")
+        paths.append( path_to_dataSetFolder / "CV_153333_ 3.tdms")
+        paths.append( path_to_dataSetFolder / "CV_151300_ 3.tdms")
+        paths.append( path_to_dataSetFolder / "CV_151725_ 3.tdms")
+        paths.append( path_to_dataSetFolder / "CV_151512_ 3.tdms")
+        self.assertEqual(paths[0].exists, True)
         
-        
-    
-       
-        
-        
+            
     
   
 
--- test/test_cv_datas.py
+++ test/test_cv_datas.py
@@ -7,24 +7,26 @@ import unittest   # The test framework
 import numpy as np
 from pathlib import Path
 
-E =np.array([1,2,3])
-paths = []
-path_to_dataSetFolder = Path(".").cwd().parent.parent / "test_data" /"CV"
-print(path_to_dataSetFolder)
-#paths.append( path_to_dataSetFolder / "CV_144913_ 3.tdms")
-paths.append( path_to_dataSetFolder / "CV_153559_ 3.tdms")
-paths.append( path_to_dataSetFolder / "CV_153541_ 3.tdms")
-paths.append( path_to_dataSetFolder / "CV_153333_ 3.tdms")
-paths.append( path_to_dataSetFolder / "CV_151300_ 3.tdms")
-paths.append( path_to_dataSetFolder / "CV_151725_ 3.tdms")
-paths.append( path_to_dataSetFolder / "CV_151512_ 3.tdms")
 
 class Test_CV_Datas(unittest.TestCase):
     
     def load_files(self):
+        
+        E =np.array([1,2,3])
+        paths = []
+        path_to_dataSetFolder = Path(".").cwd().parent.parent / "test_data" /"CV"
+        print(path_to_dataSetFolder)
+        #paths.append( path_to_dataSetFolder / "CV_144913_ 3.tdms")
+        paths.append( path_to_dataSetFolder / "CV_153559_ 3.tdms")
+        paths.append( path_to_dataSetFolder / "CV_153541_ 3.tdms")
+        paths.append( path_to_dataSetFolder / "CV_153333_ 3.tdms")
+        paths.append( path_to_dataSetFolder / "CV_151300_ 3.tdms")
+        paths.append( path_to_dataSetFolder / "CV_151725_ 3.tdms")
+        paths.append( path_to_dataSetFolder / "CV_151512_ 3.tdms")
         for path in paths:
             Path(".").exists
             self.assertEqual(path.exists, True)
+        self.assertEqual(True, True)
         
         
         
